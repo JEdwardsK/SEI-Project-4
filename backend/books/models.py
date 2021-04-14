bdcaccaf7ff17f7f48c95098c67783468a906c6b
J Edwards <jesse.edwards2@protonmail.com>
2021-04-14 18:29:44 -0400

    recreated book

---
+++ backend/books/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author =models.CharField(max_length=50)
+    cover_image =models.CharField(max_length=50)
+    ISBN =models.CharField(max_length=50)
+    is_made_into_film = models.BooleanField('Has the book been made into a film?', default=False)
+    is_made_into_series = models.BooleanField('Has the book been made into a tv series?', default=False)
+    story_overview = models.TextField(max_length= 400, null=True)
+    page_count = models.IntegerField(default= 0)
+    published_by =models.CharField(max_length=50)
+    pub_date = models.DateField('date published', null=True)
+
+
+    # RELATIONSHIPS - MANY2MANY
+    # protagonist_character =
+    # antagonist_character =
+    # supporting_characters =
+    # book_contributor =
+    # genre =
+    # RELATIONSHIPS - MANY2ONE
+
+
+    # RELATIONSHIPS - ONE2MANY
+    # book_creator =
+
+    def __str__(self):
+        return f"{self.book_name}, by {self.author}."
+
+
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # USER CREATED APPS
+    'books',
+    # 3RD PARTY APPS
+
 ]
 
 MIDDLEWARE = [
