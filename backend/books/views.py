724ae344a30bad2f5165220e95a50a4eb8872444
J Edwards <jesse.edwards2@protonmail.com>
2021-04-14 19:12:46 -0400

    added views and urls for books

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Book
+from .serializers.common import BookSerializer
+class BookListView(APIView):
+    def get(self, _request):
+        meals = Book.objects.all() # return everything from the db
+        serialized_books = BookSerializer(meals, many=True) # convert the data
+        return Response(serialized_books.data, status=status.HTTP_200_OK)
