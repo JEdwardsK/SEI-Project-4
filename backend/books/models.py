83021e949a1bf9b36f32f0c51329747edeae5e5f
J Edwards <jesse.edwards2@protonmail.com>
2021-04-15 04:11:02 -0400

    added genre seed

--- backend/books/models.py
+++ backend/books/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     author =models.CharField(max_length=50)
-    cover_image =models.CharField(max_length=50)
+    cover_image =models.CharField(max_length=50, null=True)
     ISBN =models.CharField(max_length=50)
     is_made_into_film = models.BooleanField('Has the book been made into a film?', default=False)
     is_made_into_series = models.BooleanField('Has the book been made into a tv series?', default=False)
-    story_overview = models.TextField(max_length= 400, null=True)
+    story_overview = models.TextField(max_length= 2000, null=True)
     page_count = models.IntegerField(default= 0)
     published_by =models.CharField(max_length=50)
     pub_date = models.DateField('date published', null=True)
--- backend/genres/models.py
+++ backend/genres/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Genre(models.Model):
-    genre = models.CharField(max_length=20)
+    genre = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return f"{self.genre}"
