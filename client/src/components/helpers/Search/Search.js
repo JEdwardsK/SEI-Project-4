566f511c435d29ce569c19f8c52c77db4ce7654e
J Edwards <jesse.edwards2@protonmail.com>
2021-04-16 17:40:35 -0400

    finished writing search request logic, testing required after fix 403 error

--- client/src/components/helpers/Search/Search.js
+++ client/src/components/helpers/Search/Search.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 
 const Search = () => {
-  let
+
   const [allBooks, setAllBooks] = useState(null)
 
   useEffect (() => {
@@ -13,10 +13,17 @@ const Search = () => {
     }
     getData()
   },[])
-  const validBooksSearchArray = []
+
+  if (!allBooks) return null
+
+  let validBooksSearchArray = []
+
+  // these are from the book search form data, replace accordingly
+  let formTitle, formAuthor, formIsFilm, formSearchPhrases, formGenres, formFirstName, formLastName, formIsSeries
+
   allBooks.forEach(book => {
     let matchCount = 0
-    const { title, author, is_made_into_film, is_made_into_series, story_overview, genre: genres} = book
+    const { title, author, is_made_into_film, is_made_into_series, story_overview, genre: genres, supporting_characters} = book
     if (title.includes(formTitle)) matchCount++
     if (author.includes(formAuthor)) matchCount++
     if (is_made_into_film === formIsFilm) matchCount++
@@ -30,6 +37,19 @@ const Search = () => {
     if (genres.includes(formGenre)) matchCount++
     })
 
+    //? check data structure before use main_antagonist
+    //? check data structure before use main_protagonist
+
+    // make sure the for data produces an array of first names and last names. each character checks against the first and last name arrays
+    supporting_characters.forEach(char => {
+      if(formFirstName.includes(char.first_name)) matchCount++
+      if(formLastName.includes(char.last_name)) matchCount++
+    })
+
+    // after checking all fields, if the match count is greater than 0, the search has found at least one match. We want to return the books that have matched something
+    if (matchCount > 0) {
+      validBooksSearchArray.push(book, matchCount)
+    }
   })
 
   return (
