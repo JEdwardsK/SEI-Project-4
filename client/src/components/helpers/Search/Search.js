f230428dac7af7b209e15e309edb3eb16ebcd78c
J Edwards <jesse.edwards2@protonmail.com>
2021-04-16 16:23:40 -0400

    continue search function testing

--- client/src/components/helpers/Search/Search.js
+++ client/src/components/helpers/Search/Search.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 
 const Search = () => {
-
+  let
   const [allBooks, setAllBooks] = useState(null)
 
   useEffect (() => {
@@ -13,10 +13,24 @@ const Search = () => {
     }
     getData()
   },[])
-
+  const validBooksSearchArray = []
   allBooks.forEach(book => {
+    let matchCount = 0
+    const { title, author, is_made_into_film, is_made_into_series, story_overview, genre: genres} = book
+    if (title.includes(formTitle)) matchCount++
+    if (author.includes(formAuthor)) matchCount++
+    if (is_made_into_film === formIsFilm) matchCount++
+    if (is_made_into_series === formIsSeries) matchCount++
 
-  });
+    formSearchPhrases.forEach(searchPhrase => {
+    if (story_overview.includes(searchPhrase)) matchCount++
+    })
+    // genre is an array of objects with the key genres. so to get to genres you need to genre.
+    formGenres.forEach(formGenre => {
+    if (genres.includes(formGenre)) matchCount++
+    })
+
+  })
 
   return (
 
