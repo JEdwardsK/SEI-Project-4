eed3d70678e288a2e5dfb64029e4fbe3fea9cc81
J Edwards <jesse.edwards2@protonmail.com>
2021-04-17 16:44:49 -0400

    started on genre index page, added book carousel component

---
+++ client/src/components/Books/BooksCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BooksCarousel = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BooksCarousel
--- client/src/components/Genres/GenreIndex.js
+++ client/src/components/Genres/GenreIndex.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import BooksCarousel from '../Books/BooksCarousel'
 
 const GenreIndex = () => {
+
+  const [allGenres, setAllGenres] = useState(null)
+
+
+  useEffect(() => {
+    const getGenres = async () => {
+      const { data } = await axios.get('api/genres')
+      setAllGenres(data)
+    }
+    getGenres()
+  },[])
+
+  if (!allGenres) return null
   return (
-    <div>
-      
-    </div>
+    <>
+      {allGenres.map(item => {
+        const {id, books, genre } = item
+        return (
+          <>
+          <div className="genre" key={id}>
+            <h3>{genre}</h3>
+            <div className="genre-books">
+              <BooksCarousel books={books}/>
+            </div>
+          </div>
+          </>
+        )
+      })}
+    </>
   )
 }
 
