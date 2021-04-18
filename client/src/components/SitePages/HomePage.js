c9ffa5acbc185444102643edab4920948a416f51
J Edwards <jesse.edwards2@protonmail.com>
2021-04-18 17:20:07 -0400

    login tested and working, need to test authentication, also handle logout in nav bar

--- client/src/components/SitePages/HomePage.js
+++ client/src/components/SitePages/HomePage.js
@@ -2,9 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const HomePage = () => {
   return (
-    <div>
-    <Link to='/books'>GO TO ALL BOOKS</Link>
-    </div>
+    <h1>HOME PAGE</h1>
+
   )
 }
 
