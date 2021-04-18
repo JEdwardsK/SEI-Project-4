399b0543e77c17137c24dd1ef33e7acb4cadb32e
J Edwards <jesse.edwards2@protonmail.com>
2021-04-18 17:39:15 -0400

    logout working, conditioanlly appears if the user authenticated, only disappears on refresh

--- client/src/components/SitePages/Navbar.js
+++ client/src/components/SitePages/Navbar.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { userIsAuthenticated } from '../helpers/auth'
 
 const Navbar = () => {
+  const history = useHistory()
+  const handleLogout = () => {
+    window.localStorage.removeItem('token')
+    window.alert('you have logged out, returning to homepage')
+    history.push('/')
+  }
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to ="/books">Books</Link>
-      <Link to="/genres">Genres</Link>
-      <Link to="/protagonists">Protagonists</Link>
-      <Link to="/antagonists">Antagonists</Link>
-      <Link to="/supporting_characters">Supporting Characters</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/login">Log In</Link>
-      <Link to="/search">Search</Link>
+    <div className="navbar">
+      <Link to="/"><p className="nav-element">Home</p></Link>
+      <Link to ="/books"><p className="nav-element">Books</p></Link>
+      <Link to="/genres"><p className="nav-element">Genres</p></Link>
+      <Link to="/protagonists"><p className="nav-element">Protagonists</p></Link>
+      <Link to="/antagonists"><p className="nav-element">Antagonists</p></Link>
+      <Link to="/supporting_characters"><p className="nav-element">Supporting Characters</p></Link>
+      <Link to="/profile"><p className="nav-element">Profile</p></Link>
+      <Link to="/login"><p className="nav-element">Log In</p></Link>
+      <Link to="/search"><p className="nav-element">Search</p></Link>
+      {
+        userIsAuthenticated() &&
+      <p className="nav-element" onClick={handleLogout}>Logout</p>
+      }
 
     </div>
   )
